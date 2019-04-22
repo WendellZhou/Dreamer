d65b401dfacbc1a5e8715307306a789b36b7d3b7
wendell <wendell@dream521.com>
2019-04-22 17:52:44 +0800

    01新加内容

--- src/main/java/com/dream521/dreamer/Models/MyDevelopJava.java
+++ src/main/java/com/dream521/dreamer/Models/MyDevelopJava.java
@@ -1,9 +1,7 @@
 package com.dream521.dreamer.Models;
 
 public class MyDevelopJava {
-    // 4月3日修改了最新的brunch文件内容
-	// 这个是最新添加的
-	// 这个是最新添加的2
-	// 新加远程内容3
-    // drea02 我是422新加的内容
+    // 我是01的第一行
+	// 我是01的第二行
+	// 我是01的第三行
 }
